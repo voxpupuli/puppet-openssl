0877d99648c65e0317430c91d04ede4bd87ec77f
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2011-11-04 10:22:17 +0100

    openssl::export::pkcs12 - new definition. names says it all

---
+++ manifests/definitions/openssl-export-pkcs12.pp
@@ -0,0 +1,31 @@
+/*
+
+=Definition: openssl::export::pkcs12
+Export a key pair to PKCS12 format
+
+Args:
+  $basedir   - directory where you want the export to be done. Must exists
+  $pkey      - private key
+  $cert      - certificate
+  $pkey_pass - private key password
+
+*/
+define openssl::export::pkcs12($ensure=present,
+                               $basedir,
+                               $pkey,
+                               $cert,
+                               $pkey_pass) {
+  case $ensure {
+    present: {
+      exec {"Export $name to ${basedir}/${name}.p12":
+        command => "openssl pkcs12 -export -in ${cert} -inkey ${pkey} -out ${basedir}/${name}.p12 -name ${name} -nodes -noiter -passout pass:${pkey_pass}",
+        creates => "${basedir}/${name}.p12",
+      }
+    }
+    absent: {
+      file {"${basedir}/${name}.p12":
+        ensure => absent,
+      }
+    }
+  }
+}
