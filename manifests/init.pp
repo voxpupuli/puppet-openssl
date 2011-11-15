518e4ae6d79dee429105bf2c7fdd4ca571a34751
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-15 13:05:23 +0100

    file renaming to be compliant with recommended module structure.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,37 @@
-import "classes/*.pp"
-import "definitions/*.pp"
+/*
+
+== Class: openssl
+
+Installs openssl and ensures bundled certificate list is world readable.
+
+*/
+class openssl {
+
+  package { "openssl":
+    ensure => present
+  }
+
+  if $operatingsystem == "Debian" {
+
+    package { "ca-certificates":
+      ensure => present,
+      before => [Package["openssl"], File["ca-certificates.crt"]],
+    }
+
+    exec { "update-ca-certificates":
+      refreshonly => true,
+      require     => Package["ca-certificates"],
+      before      => File["ca-certificates.crt"],
+    }
+  }
+
+  file { "ca-certificates.crt":
+    mode    => 0644,
+    owner   => "root",
+    path    => $operatingsystem ? {
+      Debian => "/etc/ssl/certs/ca-certificates.crt",
+      RedHat => "/etc/pki/tls/certs/ca-bundle.crt",
+    },
+    require => Package["openssl"],
+  }
+}
