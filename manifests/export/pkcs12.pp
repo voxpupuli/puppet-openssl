f4ee961299b15ee2c1c60e310fed760a2c1f87bd
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-11-08 23:37:04 +0100

    Make pkey optional in openssl::export::pkcs12

--- manifests/export/pkcs12.pp
+++ manifests/export/pkcs12.pp
@@ -12,13 +12,18 @@ Args:
 */
 define openssl::export::pkcs12($ensure=present,
                                $basedir,
-                               $pkey,
+                               $pkey='',
                                $cert,
                                $pkey_pass) {
   case $ensure {
     present: {
+      $in_opt = $pkey ? {
+        ''      => '',
+        default => "-inkey ${pkey}",
+      }
+
       exec {"Export $name to ${basedir}/${name}.p12":
-        command => "openssl pkcs12 -export -in ${cert} -inkey ${pkey} -out ${basedir}/${name}.p12 -name ${name} -nodes -noiter -passout pass:${pkey_pass}",
+        command => "openssl pkcs12 -export -in ${cert} ${in_opt} -out ${basedir}/${name}.p12 -name ${name} -nodes -noiter -passout pass:${pkey_pass}",
         creates => "${basedir}/${name}.p12",
       }
     }
