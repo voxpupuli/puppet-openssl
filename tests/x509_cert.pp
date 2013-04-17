e7db98314c23ccf6f160a584ab8983b5187ae674
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-17 10:11:46 +0200

    Add a tests directory

---
+++ tests/x509_cert.pp
@@ -0,0 +1,8 @@
+openssl::certificate::x509 {"foo.bar":
+  ensure       => present,
+  country      => "CH",
+  organisation => "Example.com",
+  commonname   => $fqdn,
+  base_dir     => "/tmp",
+  owner        => "www-data",
+}
