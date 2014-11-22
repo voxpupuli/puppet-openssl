acf5226e0094450b73461aebf679a23adfedc282
Cédric Jeanneret <tengu@ethack.org>
2014-11-22 14:08:57 +0100

    Allow to pass a chain cert while exporting to pkcs12 format

--- manifests/export/pkcs12.pp
+++ manifests/export/pkcs12.pp
@@ -7,23 +7,41 @@
 #   [*pkey*]      - private key
 #   [*cert*]      - certificate
 #   [*pkey_pass*] - private key password
+#   [*chaincert*] - chain certificate to include in pkcs12
 #
 define openssl::export::pkcs12(
   $basedir,
   $pkey,
   $cert,
-  $pkey_pass,
-  $ensure=present
+  $ensure    = present,
+  $chaincert = false,
+  $pkey_pass = false,
 ) {
   case $ensure {
     present: {
       $pass_opt = $pkey_pass ? {
-        ''      => '',
+        false   => '',
         default => "-passout pass:${pkey_pass}",
       }
 
+      $chain_opt = $chaincert ? {
+        false   => '',
+        default => "-chain -CAfile ${chaincert}",
+      }
+
+      $cmd = [
+        'openssl pkcs12 -export',
+        "-in ${cert}",
+        "-inkey ${pkey}",
+        "-out ${basedir}/${name}.p12",
+        "-name ${name}",
+        '-nodes -noiter',
+        $chain_opt,
+        $pass_opt,
+      ]
+
       exec {"Export ${name} to ${basedir}/${name}.p12":
-        command => "openssl pkcs12 -export -in ${cert} -inkey ${pkey} -out ${basedir}/${name}.p12 -name ${name} -nodes -noiter ${pass_opt}",
+        command => inline_template('<%= @cmd.join(" ") %>'),
         creates => "${basedir}/${name}.p12",
       }
     }
