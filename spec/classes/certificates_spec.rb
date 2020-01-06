a74c00c548101fdb2bd30a6cf2735ae19307e0a4
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-01-06 15:08:32 +0100

    Fix certificates_spec.rb

--- spec/classes/certificates_spec.rb
+++ spec/classes/certificates_spec.rb
@@ -3,19 +3,21 @@ require 'spec_helper'
 describe 'openssl::certificates' do
   let :params do
     {
-      :x509_certs => { '/path/to/certificate.crt' => { 'ensure'      => 'present',
-                                                       'password'    => 'j(D$',
-                                                       'template'    => '/other/path/to/template.cnf',
-                                                       'private_key' => '/there/is/my/private.key',
-                                                       'days'        => 4536,
-                                                       'force'       => false,},
-                       '/a/other/certificate.crt' => { 'ensure'      => 'present', },
+      :x509_certs => { '/path/to/certificate.crt' => { 'ensure'       => 'present',
+                                                       'country'      => 'CH',
+                                                       'organization' => 'Corp',
+                                                       'commonname'   => 'foo',
+                                                       'password'     => 'j(D$',
+                                                       'days'         => 4536,
+                                                       'force'        => false,},
+                       '/a/other/certificate.crt' => { 'ensure'       => 'present',
+                                                       'country'      => 'FR',
+                                                       'organization' => 'OtherCorp',
+                                                       'commonname'   => 'bar',},
                      }
     }
   end
   it 'has certs' do
-    expect {
-      is_expected.to have_openssl__certificate__x509_count(2)
-    }
+    is_expected.to have_openssl__certificate__x509_resource_count(2)
   end
 end
