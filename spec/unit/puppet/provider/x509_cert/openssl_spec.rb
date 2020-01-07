bd7d2a8c696f2d60d2686df3d0d02f17224e07df
Raphaël Pinson <raphael.pinson@camptocamp.com>
2020-01-07 14:41:22 +0100

    Fix RSpec/AnyInstance

--- spec/unit/puppet/provider/x509_cert/openssl_spec.rb
+++ spec/unit/puppet/provider/x509_cert/openssl_spec.rb
@@ -7,18 +7,19 @@ provider_class = Puppet::Type.type(:x509_cert).provider(:openssl)
 describe 'The openssl provider for the x509_cert type' do
   let(:path) { '/tmp/foo.crt' }
   let(:resource) { Puppet::Type::X509_cert.new(path: path) }
+  let(:cert) { OpenSSL::X509::Certificate.new }
 
   context 'when not forcing key' do
     it 'exists? should return true if certificate exists and is synced' do
       allow(File).to receive(:read)
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
       c = OpenSSL::X509::Certificate.new # Fake certificate for mocking
       allow(OpenSSL::X509::Certificate).to receive(:new).and_return(c)
       expect(resource.provider.exists?).to eq(true)
     end
 
     it 'exists? should return false if certificate does not exist' do
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false) # rubocop:disable RSpec/AnyInstance
       expect(resource.provider.exists?).to eq(false)
     end
 
@@ -53,34 +54,32 @@ describe 'The openssl provider for the x509_cert type' do
     it 'exists? should return true if certificate exists and is synced' do
       resource[:force] = true
       allow(File).to receive(:read)
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
-      c = OpenSSL::X509::Certificate.new # Fake certificate for mocking
-      allow(OpenSSL::X509::Certificate).to receive(:new).and_return(c)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
+      allow(OpenSSL::X509::Certificate).to receive(:new).and_return(cert)
       allow(OpenSSL::PKey::RSA).to receive(:new)
-      allow_any_instance_of(OpenSSL::X509::Certificate).to receive(:check_private_key).and_return(true)
+      expect(cert).to receive(:check_private_key).and_return(true)
       expect(resource.provider.exists?).to eq(true)
     end
 
     it 'exists? should return false if certificate exists and is not synced' do
       resource[:force] = true
       allow(File).to receive(:read)
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
-      c = OpenSSL::X509::Certificate.new # Fake certificate for mocking
-      allow(OpenSSL::X509::Certificate).to receive(:new).and_return(c)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
+      allow(OpenSSL::X509::Certificate).to receive(:new).and_return(cert)
       allow(OpenSSL::PKey::RSA).to receive(:new)
-      allow_any_instance_of(OpenSSL::X509::Certificate).to receive(:check_private_key).and_return(false)
+      expect(cert).to receive(:check_private_key).and_return(false)
       expect(resource.provider.exists?).to eq(false)
     end
 
     it 'exists? should return false if certificate does not exist' do
       resource[:force] = true
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false) # rubocop:disable RSpec/AnyInstance
       expect(resource.provider.exists?).to eq(false)
     end
   end
 
   it 'deletes files' do
-    allow_any_instance_of(Pathname).to receive(:delete)
+    allow_any_instance_of(Pathname).to receive(:delete) # rubocop:disable RSpec/AnyInstance
     resource.provider.destroy
   end
 end
--- spec/unit/puppet/provider/x509_request/openssl_spec.rb
+++ spec/unit/puppet/provider/x509_request/openssl_spec.rb
@@ -6,15 +6,16 @@ provider_class = Puppet::Type.type(:x509_request).provider(:openssl)
 describe 'The openssl provider for the x509_request type' do
   let(:path) { '/tmp/foo.csr' }
   let(:resource) { Puppet::Type::X509_request.new(path: path) }
+  let(:cert) { OpenSSL::X509::Request.new }
 
   context 'when not forcing key' do
     it 'exists? should return true if csr exists' do
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
       expect(resource.provider.exists?).to eq(true)
     end
 
     it 'exists? should return false if csr exists' do
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false) # rubocop:disable RSpec/AnyInstance
       expect(resource.provider.exists?).to eq(false)
     end
 
@@ -47,34 +48,32 @@ describe 'The openssl provider for the x509_request type' do
     it 'exists? should return true if certificate exists and is synced' do
       resource[:force] = true
       allow(File).to receive(:read)
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
-      c = OpenSSL::X509::Request.new # Fake certificate for mocking
-      allow(OpenSSL::X509::Request).to receive(:new).and_return(c)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
+      allow(OpenSSL::X509::Request).to receive(:new).and_return(cert)
       allow(OpenSSL::PKey::RSA).to receive(:new)
-      allow_any_instance_of(OpenSSL::X509::Request).to receive(:verify).and_return(true)
+      expect(cert).to receive(:verify).and_return(true)
       expect(resource.provider.exists?).to eq(true)
     end
 
     it 'exists? should return false if certificate exists and is not synced' do
       resource[:force] = true
       allow(File).to receive(:read)
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true)
-      c = OpenSSL::X509::Request.new # Fake certificate for mocking
-      allow(OpenSSL::X509::Request).to receive(:new).and_return(c)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(true) # rubocop:disable RSpec/AnyInstance
+      allow(OpenSSL::X509::Request).to receive(:new).and_return(cert)
       allow(OpenSSL::PKey::RSA).to receive(:new)
-      allow_any_instance_of(OpenSSL::X509::Request).to receive(:verify).and_return(false)
+      expect(cert).to receive(:verify).and_return(false)
       expect(resource.provider.exists?).to eq(false)
     end
 
     it 'exists? should return false if certificate does not exist' do
       resource[:force] = true
-      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+      allow_any_instance_of(Pathname).to receive(:exist?).and_return(false) # rubocop:disable RSpec/AnyInstance
       expect(resource.provider.exists?).to eq(false)
     end
   end
 
   it 'deletes files' do
-    allow_any_instance_of(Pathname).to receive(:delete)
+    allow_any_instance_of(Pathname).to receive(:delete) # rubocop:disable RSpec/AnyInstance
     resource.provider.destroy
   end
 end
