bc1eca5a705ee1fa8507086987e54584f586db98
Peter Tiggerdine <ptiggerdine@users.noreply.github.com>
2020-10-15 19:48:48 +1000

    Added convert from der to pem as well.

--- manifests/export/pem_cert.pp
+++ manifests/export/pem_cert.pp
@@ -4,30 +4,39 @@
 #
 # == Parameters
 #   [*pfx_cert*]  - PFX certificate/key container
+#   [*der_cert*]  - DER certificate
 #   [*pem_cert*]  - PEM/x509 certificate
 #   [*in_pass*]   - PFX password
 #
 define openssl::export::pem_cert(
   Stdlib::Absolutepath      $pfx_cert,
+  Stdlib::Filesource        $der_cert,
   Stdlib::Absolutepath      $pem_cert = $title,
   Enum['present', 'absent'] $ensure   = present,
   Optional[String]          $in_pass  = undef,
 ) {
-  if $ensure == 'present' {
+  if $der_cert ? {
+    $sslmodule = 'x509',
+    $in_cert = $der_cert,
+  } else {
+    $sslmodule = 'pkcs12'
+    $in_cert = $pfx_cert,
     $passin_opt = $in_pass ? {
       undef   => '',
-      default => "-passin pass:'${in_pass}'",
+      default => "-nokeys -passin pass:'${in_pass}'",
     }
-
+    
+  }
+  
+  if $ensure == 'present' {
     $cmd = [
-      'openssl pkcs12',
-      "-in ${pfx_cert}",
+      'openssl $sslmodule',
+      "-in ${in_cert}",
       "-out ${pem_cert}",
-      '-nokeys',
       $passin_opt,
     ]
 
-    exec {"Export ${pfx_cert} to ${pem_cert}":
+    exec {"Export ${in_cert} to ${pem_cert}":
       command => inline_template('<%= @cmd.join(" ") %>'),
       path    => $::path,
       creates => $pem_cert,
