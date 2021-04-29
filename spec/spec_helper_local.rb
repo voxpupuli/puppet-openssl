2b617f42574f570dcc16f27ac380694fa2683ec0
Tibor Répási <rtib@users.noreply.github.com>
2021-04-29 14:43:44 +0200

    refactor fixture code to narrower scope

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,6 +1,3 @@
-require 'webmock/rspec'
-require 'openssl'
-
 # HACK: to enable all the expect syntax (like allow_any_instance_of) in rspec-puppet examples
 RSpec::Mocks::Syntax.enable_expect(RSpec::Puppet::ManifestMatchers)
 
@@ -16,22 +13,6 @@ RSpec.configure do |config|
     Facter.clear_messages
 
     RSpec::Mocks.setup
-
-    test_keys = OpenSSL::PKey::RSA.new(2049)
-    test_cert = OpenSSL::X509::Certificate.new
-    test_cert.version = 2
-    test_cert.serial = 1
-    test_cert.subject = OpenSSL::X509::Name.parse('/CN=test-cert.example.org')
-    test_cert.public_key = test_keys.public_key
-    test_cert.not_before = Time.now
-    test_cert.not_after = test_cert.not_before + 3600 # 1 hour
-    test_cert.sign(test_keys, OpenSSL::Digest::SHA256.new)
-
-    resp_header = { 'Content-Type': 'application/x-x509-ca-cert' }
-    stub_request(:get, 'http://example.org/cert.der')
-      .to_return(status: 200, body: test_cert.to_der, headers: resp_header)
-    stub_request(:get, 'http://example.org/cert.pem')
-      .to_return(status: 200, body: test_cert.to_pem, headers: resp_header)
   end
 
   config.after :each do
--- spec/unit/puppet/provider/cert_file/posix_spec.rb
+++ spec/unit/puppet/provider/cert_file/posix_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
 require 'puppet/type/cert_file'
 require 'pathname'
+require 'webmock/rspec'
+require 'openssl'
 
 describe 'The POSIX provider for type cert_file' do
+  before(:each) do
+    test_keys = OpenSSL::PKey::RSA.new(2049)
+    test_cert = OpenSSL::X509::Certificate.new
+    test_cert.version = 2
+    test_cert.serial = 1
+    test_cert.subject = OpenSSL::X509::Name.parse('/CN=test-cert.example.org')
+    test_cert.public_key = test_keys.public_key
+    test_cert.not_before = Time.now
+    test_cert.not_after = test_cert.not_before + 3600 # 1 hour
+    test_cert.sign(test_keys, OpenSSL::Digest::SHA256.new)
+  
+    resp_header = { 'Content-Type': 'application/x-x509-ca-cert' }
+    stub_request(:get, 'http://example.org/cert.der')
+      .to_return(status: 200, body: test_cert.to_der, headers: resp_header)
+    stub_request(:get, 'http://example.org/cert.pem')
+      .to_return(status: 200, body: test_cert.to_pem, headers: resp_header)
+  end
+  
   let(:path) { '/tmp/test.pem' }
   let(:source) { 'http://example.org/cert.der' }
   let(:resource) { Puppet::Type::Cert_file.new(path: path, source: source) }
