f3965aa71e8ac3e5008a9e7b9bee256211d572dd
Peter Tiggerdine <ptiggerdine@users.noreply.github.com>
2021-05-05 04:42:44 +1000

    Allow DER certificates to be converted to PEM format (#122)
    * Added convert from der to pem as well.
    
    * fix lint and typo mistakes
    
    * Working der to pem with unit tests passing.
    
    * Add Documentation for to complete RFE: 121
    
    * Update manifests/export/pem_cert.pp
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update manifests/export/pem_cert.pp
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update manifests/export/pem_cert.pp
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update manifests/export/pem_cert.pp
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    * Update spec/defines/openssl_export_pem_cert_spec.rb
    
    Co-authored-by: ptiggerdine <ptiggerdine [at] gmail.com>
    Co-authored-by: Raphaël Pinson <github+aem1eeshi1@raphink.net>

--- manifests/export/pem_cert.pp
+++ manifests/export/pem_cert.pp
@@ -4,30 +4,54 @@
 #
 # == Parameters
 #   [*pfx_cert*]  - PFX certificate/key container
+#   [*der_cert*]  - DER certificate
 #   [*pem_cert*]  - PEM/x509 certificate
 #   [*in_pass*]   - PFX password
 #
-define openssl::export::pem_cert(
-  Stdlib::Absolutepath      $pfx_cert,
-  Stdlib::Absolutepath      $pem_cert = $title,
-  Enum['present', 'absent'] $ensure   = present,
-  Optional[String]          $in_pass  = undef,
-) {
-  if $ensure == 'present' {
-    $passin_opt = $in_pass ? {
-      undef   => '',
-      default => "-passin pass:'${in_pass}'",
-    }
+define openssl::export::pem_cert (
+  Enum['present', 'absent']       $ensure   = present,
+  Stdlib::Absolutepath            $pem_cert = $title,
+  Optional[Stdlib::Absolutepath]  $pfx_cert = undef,
+  Optional[Stdlib::Absolutepath]  $der_cert = undef,
+  Optional[String]                $in_pass  = undef,
 
+) {
+  #local variables
+
+  # If ensure = present and  der_cert and $pfx_cert as being specified, then throw error
+    if $ensure == present and !$der_cert and !$pfx_cert {
+    fail('Parameter Error: either pfx_cert or der_cert must be specified')
+  }
+
+  if $der_cert and $pfx_cert {
+    fail('Parameter Error: pfx_cert and der_cert are mutually-exclusive')
+  }
+
+  if $der_cert {
+    $sslmodule = 'x509'
+    $in_cert   = $der_cert
+    $module_opt  = '-inform DER'
+  } else {
+    $sslmodule  = 'pkcs12'
+    $in_cert    = $pfx_cert
+    $module_opt   = ''
+  }
+
+  $passin_opt = $in_pass ? {
+    undef   => '',
+    default => "-nokeys -passin pass:'${in_pass}'",
+  }
+
+  if $ensure == 'present' {
     $cmd = [
-      'openssl pkcs12',
-      "-in ${pfx_cert}",
+      "openssl ${sslmodule}",
+      $module_opt,
+      "-in ${in_cert}",
       "-out ${pem_cert}",
-      '-nokeys',
       $passin_opt,
     ]
 
-    exec {"Export ${pfx_cert} to ${pem_cert}":
+    exec { "Export ${in_cert} to ${pem_cert}":
       command => inline_template('<%= @cmd.join(" ") %>'),
       path    => $::path,
       creates => $pem_cert,
