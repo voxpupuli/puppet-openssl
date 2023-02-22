a8db849aa80ec942e22b5b1440624ffe3c0a2930
markuszilch <markus.zilch@tik.uni-stuttgart.de>
2023-02-22 15:54:05 +0100

    corrected parameter propagation to openssl comand for ca and cakey

--- lib/puppet/provider/x509_cert/openssl.rb
+++ lib/puppet/provider/x509_cert/openssl.rb
@@ -77,8 +77,8 @@ Puppet::Type.type(:x509_cert).provide(:openssl) do
     ]
     options << ['-passin', "pass:#{resource[:password]}"] if resource[:password]
     options << ['-extensions', 'req_ext'] if resource[:req_ext] != :false
-    options << ['-CA', 'ca'] if resource[:ca]
-    options << ['-CAkey', 'cakey'] if resource [:cakey]
+    options << ['-CA', resource[:ca]] if resource[:ca]
+    options << ['-CAkey', resource[:cakey]] if resource[:cakey]
     openssl options
   end
 
