3ffeceaed983db282c3a8940a9fe0b62bcc18a13
Tim Meusel <tim@bastelfreak.de>
2024-07-18 17:29:45 +0200

    Update examples/x509_cert.pp
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- examples/x509_cert.pp
+++ examples/x509_cert.pp
@@ -6,5 +6,6 @@ openssl::certificate::x509 { 'foo.example.com':
   base_dir     => '/tmp',
   owner        => 'nobody',
   password     => 'mahje1Qu',
+  # This is just to speed up CI - use 2048 or more in production
   key_size     => 1024,
 }
