a749147cb5d0e155b5fb2fd99f3052cc341d5372
Marcus Poller <marcus.poller@betadots.de>
2025-01-10 09:57:30 +0100

    add example for OpenSSL PEM key

---
+++ examples/x509_pkcs12_pemkey.pp
@@ -0,0 +1,20 @@
+contain openssl
+
+openssl::certificate::x509 { 'sample_x509':
+  ensure   => present,
+  base_dir => '/tmp',
+  key_size => 1024, #entropy in CI is limited
+}
+
+openssl::export::pkcs12 { 'export.pkcs12':
+  ensure  => 'present',
+  basedir => '/tmp',
+  pkey    => '/tmp/sample_x509.key',
+  cert    => '/tmp/sample_x509.crt',
+}
+
+openssl::export::pem_key { 'key-UUID':
+  ensure   => present,
+  pfx_cert => '/tmp/export.pkcs12.p12',
+  pem_key  => '/tmp/key.pem',
+}
