b36ef28b53f61094a0412c7787b06ad808f8c798
Tim Meusel <tim@bastelfreak.de>
2025-02-10 13:43:56 +0100

    openssl::certificate::pkcs12: Add test for x509 without password

--- examples/export_pkcs12_from_key.pp
+++ examples/export_pkcs12_from_key.pp
@@ -18,3 +18,22 @@ openssl::certificate::x509 { 'foo.example.com':
   in_pass  => 'mahje1Qu',
   out_pass => 'mahje1Qu',
 }
+
+# same as above, just no password for the X509
+openssl::certificate::x509 { 'foo2.example.com':
+  ensure       => present,
+  country      => 'CH',
+  organization => 'Example.com',
+  commonname   => 'foo2.example.com',
+  base_dir     => '/tmp',
+  owner        => 'nobody',
+  # This is just to speed up CI - use 2048 or more in production
+  key_size     => 1024,
+}
+-> openssl::export::pkcs12 { 'export2.pkcs12':
+  ensure   => 'present',
+  basedir  => '/tmp',
+  pkey     => '/tmp/foo2.example.com.key',
+  cert     => '/tmp/foo2.example.com.crt',
+  out_pass => 'mahje1Qu',
+}
